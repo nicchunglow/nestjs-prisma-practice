ff805210a86aa4e6f46191dae096dd8828c049d3
nicchunglow <iamnicholaschung@gmail.com>
2024-02-07 17:39:52 +0800

    Add user service with joi schema and test

--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -1,9 +1,10 @@
 // services/user.service.spec.ts
 
+import { User } from '@prisma/client'
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserService } from './user.service'
 import { PrismaService } from '../../prisma.service'
-import { User, Prisma } from '@prisma/client'
+import { UnprocessableEntityException } from '@nestjs/common'
 
 describe('UserService', () => {
   let userService: UserService
@@ -18,19 +19,50 @@ describe('UserService', () => {
     prismaService = module.get<PrismaService>(PrismaService)
   })
 
-  describe('createUser', () => {
-    it('should create a user', async () => {
-      const userData = {
-        name: 'Alice',
-        email: 'alice@example.com',
-      }
-      const expectedResult: User = { id: 2, ...userData }
+  const userData = {
+    name: 'TestUser',
+    email: 'test@email.gov',
+    agency: 'ACRA',
+    description: 'I am a test officer',
+    acceptTerms: true,
+  }
+  it('should create a user', async () => {
+    jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null)
 
-      jest.spyOn(prismaService.user, 'create').mockResolvedValue(expectedResult)
+    jest
+      .spyOn(prismaService.user, 'create')
+      .mockResolvedValue({ id: 1, ...userData })
 
-      const result = await userService.createUser(userData)
+    const createdUser: User = await userService.createUser(userData)
 
-      expect(result).toEqual(expectedResult)
-    })
+    expect(createdUser).toBeDefined()
+    expect(createdUser.id).toEqual(1)
+    expect(createdUser.name).toEqual('TestUser')
+    expect(createdUser.email).toEqual('test@email.gov')
+  })
+
+  it('should throw "Email already exist" for duplicate email', async () => {
+    jest
+      .spyOn(prismaService.user, 'findFirst')
+      .mockResolvedValue({ id: 1, ...userData })
+
+    await expect(userService.createUser(userData)).rejects.toThrow(
+      'Email already exist'
+    )
+  })
+
+  it('should throw for invalid data', async () => {
+    const userData = {
+      name: '',
+      email: 'test@email.gov',
+      agency: 'ACRA',
+      description: 'I am a test officer',
+      acceptTerms: false,
+    }
+    jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null)
+
+    await expect(userService.createUser(userData)).rejects.toThrow(
+      'name should be valid format'
+    )
   })
 })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,36 @@
-import { Injectable } from '@nestjs/common'
-import { PrismaService } from '../../prisma.service'
 import { User, Prisma } from '@prisma/client'
+import { Injectable, UnprocessableEntityException } from '@nestjs/common'
+import { PrismaService } from '../../prisma.service'
+import { userSchema } from '../schemas/user.schema'
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    })
+    try {
+      await userSchema.validateAsync(data)
+
+      const userEmail = await this.prisma.user.findFirst({
+        where: { email: data.email },
+        select: { email: true },
+      })
+
+      if (userEmail) {
+        throw new UnprocessableEntityException('Email already exists')
+      }
+
+      return this.prisma.user.create({
+        data,
+      })
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new UnprocessableEntityException(
+          'Error creating user: ' + err.message
+        )
+      }
+
+      throw new Error(err.message)
+    }
   }
 }
